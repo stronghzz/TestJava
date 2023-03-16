6d80b3eca7ab5b08d7ad88d6a15cf8a316ff7a57
stronghzz <760767745@qq.com>
2023-03-16 19:35:46 +0800

    test1

---
+++ testpro01/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    //冒泡排序
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//创建一个类Tools,实现冒泡排序
+class MyTools {
+    public void bubble(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+
+        }
+    }
+}
