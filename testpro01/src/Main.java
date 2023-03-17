787a3a87555afa391fa8cb1a2df5efae71b20cdc
stronghzz <760767745@qq.com>
2023-03-17 19:51:03 +0800

    test

--- testpro01/src/Main.java
+++ testpro01/src/Main.java
@@ -1,16 +1,28 @@
 public class Main {
     //冒泡排序
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyTools t = new MyTools();
+        int[] arr = {10, -2, 15, 2, 3, 5, 20};
+        //System.out.println(arr.length);
+        t.bubble(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
 
 //创建一个类Tools,实现冒泡排序
 class MyTools {
-    public void bubble(int[] arr){
-        for(int i = 0; i < arr.length - 1; i++)
-        {
-
+    public void bubble(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
     }
 }
