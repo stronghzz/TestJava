1f53b9a96fe59a574af34e95abfe1a9b28d10a65
stronghzz <760767745@qq.com>
2023-03-19 21:04:24 +0800

    test

---
+++ test20230318/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+        while(beerNum > 0){
+            if(beerNum == 1){
+                word = "bottle";
+            }
+            System.out.println(beerNum + " " + word +" of beer on the wall");
+            System.out.println(beerNum+ " " + word + " of beer.");
+            System.out.println("take one down");
+            System.out.println("Pass it down");
+            beerNum = beerNum - 1;
+            if (beerNum > 0){
+                System.out.println(beerNum + " " + word + " of beer ont the wall");
+            }else{
+                System.out.println("No more bottles of beer on the wall");
+            }
+        }
+        /*int[] arr = {10, 2};
+        char a = 'c';
+        String str = "abc";
+        System.out.print(a + "\t");
+        for (int i = 0; i < 2; ++i) {
+            System.out.print(arr[i] + "\t");
+        }
+        testclass t1 = new testclass();
+        t1.test();
+        System.out.println("Hello world!");*/
+    }
+}
+
+class testclass {
+    public void test(){
+        System.out.print(a + "\t");
+    }
+    int a = 1;
+}
